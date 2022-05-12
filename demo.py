69a029b3e3a03fa07af9a32067a5be5fadd4a0f7
qilei123 <qileimail123@gmail.com>
2022-05-12 15:59:42 -0400

    test

--- demo.py
+++ demo.py
@@ -5,7 +5,8 @@ def main():
     data_root = "/home/qilei/.TEMP/TEETH3/"
     json_ann = "train_1_3_crop.json"
     coco = COCO(os.path.join(data_root,"annotations", json_ann))
-    for img in coco.imgs:
+    for img_id in coco.imgs:
+        img = coco.imgs[img_id]
         img_dir = os.path.join(data_root,"images_crop1",img["file_name"])
         output_dir = os.path.join(data_root,"images_crop_enhance",img["file_name"])
         ar = AlgorithmRunnerX("clahe",img_dir,"",output_dir)
